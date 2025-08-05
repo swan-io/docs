eb616706628b69583da60669dda2ae6beb676951
Maxence Busson <maxence.busson@swan.io>
2025-08-05 17:59:52 +0200

    Applied across the docs

--- src/components/FieldRequirements/index.js
+++ src/components/FieldRequirements/index.js
@@ -61,16 +61,16 @@ export const Yes = () => (
 export const FieldRequirementsLegend = () => (
   <div className="legend field-requirements-legend">
     <h4>Field Requirements Legend</h4>
-    <div className="legend-item"><Req /> <strong>Required:</strong> Field must be completed</div>
-    <div className="legend-item"><Cnd /> <strong>Conditional:</strong> Field is required only in specific situations</div>
-    <div className="legend-item"><Opt /> <strong>Optional:</strong> Field can be left blank; may have a default value</div>
+    <div className="legend-item"><Req /> <strong>Required:</strong> must be completed.</div>
+    <div className="legend-item"><Cnd /> <strong>Conditional:</strong> required only in specific situations.</div>
+    <div className="legend-item"><Opt /> <strong>Optional:</strong> isn't required; may have a default value.</div>
   </div>
 );
 
 export const SupportStatusLegend = () => (
   <div className="legend support-status-legend">
     <h4>Support Status Legend</h4>
-    <div className="legend-item"><Supported /> <strong>Supported:</strong> Feature is fully available</div>
-    <div className="legend-item"><Unsupported /> <strong>Unsupported:</strong> Feature is not available</div>
+    <div className="legend-item"><Supported /> <strong>Supported:</strong> Feature is available.</div>
+    <div className="legend-item"><Unsupported /> <strong>Unsupported:</strong> Feature is not available.</div>
   </div>
 );

fb920323082a307daf04387613c2f37362696e9c
Max B. <17831738+max-koro@users.noreply.github.com>
2025-09-03 16:50:38 +0200

    Doc 1246/preview docs demo (#390)
    * Create index.mdx
    
    * Create verification-of-payee.mdx
    
    * First iteration
    
    * Improvements and restructuring
    
    * Auto frontmatter import, removed hardcoded list
    
    * Improved readability & conciseness
    
    * Final restyling
    
    * Removed category + added timeline prefix
    
    * Improved disclaimer & added to preview tab
    
    * Improved tabs styling
    
    * Updated all VoP references throughout
    
    * Doc 1251/implement upcoming badge type styling on preview docs (#392)
    
    * Built belgian ibans page
    
    * DOC-1251/implement-upcoming-badge-type-styling-on-preview-docs
    
    * DOC-1250/preview-docs-implement-item-ordering (#394)
    
    * Apply suggestions from code review: Belgian IBANs
    
    * Apply suggestions from code review: Preview Docs
    
    * Update sidebars.js
    
    * Apply suggestions from code review
    
    * Update docs/preview/index.mdx
    
    * Preview card insurance
    
    * Removed Upcoming feature type category (#398)
    
    * Doc 1256/preview docs transition from the tabs model to a full docs section in (#399)
    
    * Cleanup - keeping only migrated hierarchy but removing tabs integration
    
    * Added top navbar entry + improved hierarchy
    
    * Improved index + added banner + improved sidebar styling + increased preview section awareness
    
    * Update index.mdx
    
    * Fixing card insurance migration links
    
    * Update card-insurance.mdx
    
    * Styling top navbar buttons
    
    * Slight phrasing edits
    
    * Belgian IBANs review update
    
    * Nico's restyle request
    
    * Update index.js
    
    * Update index.js
    
    * Re-adding BE IBAN Req disclaimers
    
    * Update index.mdx
    
    * Update belgian-ibans.mdx
    
    * Update belgian-ibans.mdx
    
    * Table fix
    
    * Update belgian-ibans.mdx
    
    * Racene feedback
    
    * Added many more details to belgian IBAN tables
    
    * Update belgian-ibans.mdx
    
    * Removing Belgian IBANs
    
    * Revert "Removing Belgian IBANs"
    
    This reverts commit fa8ee9847cb9d13c670053d9e1772cb2f215b507.
    
    * Alina's feedback
    
    * Update belgian-ibans.mdx
    
    * Update belgian-ibans.mdx
    
    * Update custom.css
    
    ---------
    
    Co-authored-by: ntombing <mantombingoloyi@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -754,6 +754,7 @@ module.exports = {
       collapsed: false,
       items: [
         'preview/verification-of-payee',
+        'preview/belgian-ibans',
         // Add more preview features here
       ],
     },

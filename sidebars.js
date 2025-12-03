6e27598bc1bdbd000f0aec9cc6ab5e6ccbd7412c
Maxence Busson <maxence.busson@swan.io>
2025-12-03 14:19:34 +0100

    Fixing MCP section

--- sidebars.js
+++ sidebars.js
@@ -687,7 +687,7 @@ module.exports = {
             "developers/tools/testing-api",
             "developers/tools/event-simulator",
             "developers/tools/sandbox-users",
-            "developers/tools/mcp-server",
+            "developers/tools/mcp-servers",
             "developers/tools/postman-collection",
           ],
         },
